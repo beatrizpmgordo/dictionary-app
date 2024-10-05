9f71c2c0892938aeafb6dbd21799588f94cd45eb
beatrizpmgordo <beatriz.gordo@hotmail.com>
2024-10-05 14:53:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="SheCodes-logo img-fluid"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
