5d12f8cfb1189c673b7ca643034c6645b1e42fb1
beatrizpmgordo <beatriz.gordo@hotmail.com>
2024-10-05 15:44:45 +0100

    Built de initial search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="SheCodes-logo img-fluid"
-          alt="logo"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo}
+            className="SheCodes-logo img-fluid"
+            alt="logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Beatriz Gordo</small>
+        </footer>
+      </div>
     </div>
   );
 }
